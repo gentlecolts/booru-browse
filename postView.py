95cda920adf1ca2c4b73f9e00a9b3210af688e2a
Jack Phoebus <jmp1996.jp@gmail.com>
2017-08-11 17:47:57 -0400

    can display clickable sources
    tags should work but dont for some reason though

--- postView.py
+++ postView.py
@@ -1,67 +1,68 @@
 import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version('WebKit', '3.0')
-from gi.repository import Gtk, GObject, WebKit
+from gi.repository import Gtk, WebKit
 
-class queuedBar(Gtk.ProgressBar):
-	def __init__(self):
-		super(queuedBar, self).__init__()
-		self.tasks=[]
-		self.set_show_text("Caching Thumbnails")
+#tagkeys=['artist', 'tags']
+tagkeys=['tags']
+
+class tagDisplay(Gtk.ScrolledWindow):
+	def __init__(self, tags):
+		super(tagDisplay, self).__init__()
+		self.tags=tags
+		self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 		
-		self.show()
-		def watcher():
-			#print("watching...")
-			if len(self.tasks):
-				#print("have tasks, updating")
-				self.show()
-				lis=self.tasks[0]
-				if lis[0]<lis[1]:
-					#print("progress: {}/{}".format(lis[0], lis[1]))
-					self.set_fraction(lis[0]/lis[1])
-				else:
-					#print("top task finished")
-					self.tasks.pop(0)
-			else:
-				#print("no tasks")
-				self.hide()
-			return True
-		GObject.idle_add(watcher)
-		self.refresh=watcher
+		port=Gtk.Viewport()
+		port.add(self.box)
+		self.add(port)
 	
-	def queue(self, proglist):
-		print("queued task")
-		self.tasks.append(proglist)
-		"""proglist is just a list of [downloaded,total]"""
-
-#extra stuff from tileView's progress bar, might be useful
-"""
-buf=bytes()
-
-#tracking for length
-length=response.getheader('content-length')
-if length:
-	length=int(length)
-	progress=[0, length]
-	#progressbar.queue(progress)
-	blocksize=max(4096, length//100)
-else:
-	progress=None
-	blocksize=4096
-
-while True:
-	read=response.read(blocksize)
-	if read:
-		buf+=read
+	def setContent(self, content):
+		"""add stuff to the widget"""
+		#clean anything currently here
+		children=self.box.get_children()
+		for child in children:
+			self.box.remove(child)
 		
-		if progress:
-			progress[0]=len(buf)
-	else:
-		if progress:
-			progress[0]=progress[1]
-		break;
-loader.write(buf)
-"""
+		from pprint import pprint
+		pprint(content)
+		for category, tags in content.items():
+			self.box.add(Gtk.Label(category))
+			if type(tags) is str:
+				tags=tags.split()
+			for tag in tags:
+				button=Gtk.LinkButton.new_with_label(tag, tag)
+				def click(button):
+					self.tags.addTags([button.uri])
+					return True#needed to override default click behavior
+				button.connect('activate-link', click)
+				
+				self.box.pack_start(button, expand=False, fill=True, padding=0)
+				
+				#print("added {}:{}".format(category, tag))
+		self.show_all()
+
+class sourceDisplay(Gtk.Box):
+	def __init__(self):
+		super(sourceDisplay, self).__init__(orientation=Gtk.Orientation.VERTICAL)
+	
+	def setContent(self, content):
+		"""add as many Gtk.LinkButton to the widget as needed"""
+		#clean anything currently here
+		children=self.get_children()
+		for child in children:
+			self.remove(child)
+		
+		#add new content
+		for source in content:
+			button=Gtk.LinkButton.new_with_label(source, source)
+			self.pack_start(button, expand=False, fill=True, padding=0)
+
+class commentDisplay(Gtk.ScrolledWindow):
+	def __init__(self):
+		super(commentDisplay, self).__init__()
+	
+	def setContent(self, content):
+		"""add stuff to the widget"""
 
 class postView(Gtk.Box):
 	"""settings and stuff"""
@@ -70,18 +71,27 @@ class postView(Gtk.Box):
 		super(postView, self).__init__(orientation=Gtk.Orientation.VERTICAL)
 		
 		#comments, tags, and sources
-		#TODO: implement
+		self.sources=sourceDisplay()
+		self.comments=commentDisplay()
+		self.tags=tagDisplay(booruWidget.search.tags)
 		
 		#web view for displaying media
 		self.content=WebKit.WebView()
 		
+		#pull content together
 		sw=Gtk.ScrolledWindow()
 		sw.add(self.content)
 		sw.show_all()
 		
-		#pull content together
+		midbox=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+		midbox.pack_start(sw, expand=True, fill=True, padding=0)
+		midbox.pack_start(self.sources, expand=False, fill=True, padding=0)
+		midbox.show()
+		
 		contentBox=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-		contentBox.pack_start(sw, expand=True, fill=True, padding=0)
+		contentBox.pack_start(self.tags, expand=False, fill=True, padding=0)
+		contentBox.pack_start(midbox, expand=True, fill=True, padding=0)
+		contentBox.pack_start(self.comments, expand=False, fill=True, padding=0)
 		contentBox.show()
 		
 		#pack everything into a neat box
@@ -93,15 +103,24 @@ class postView(Gtk.Box):
 		backbtn.connect("clicked", lambda btn:booruWidget.closeImage())
 		
 		#TODO: make this accept a "target", switch should hide/show the target
-		def makeToggle(name):
+		def makeToggle(name, target):
 			frame=Gtk.Frame(label=name)
 			button=Gtk.Switch()
 			frame.add(button)
+			
+			def onswitch(switch, param):
+				if switch.get_active():
+					target.show_all()
+				else:
+					target.hide()
+			
+			button.connect("notify::active",onswitch)
+			
 			return (frame, button)
 		
-		(tagframe, self.tagSwitch)=makeToggle("Tags")
-		(commentframe, self.commentSwitch)=makeToggle("Comments")
-		(sourceframe, self.sourceSwitch)=makeToggle("Source")
+		(tagframe, self.tagSwitch)=makeToggle("Tags", self.tags)
+		(commentframe, self.commentSwitch)=makeToggle("Comments", self.comments)
+		(sourceframe, self.sourceSwitch)=makeToggle("Source", self.sources)
 		
 		bottomBar=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 		bottomBar.pack_start(backbtn, expand=False, fill=True, padding=0)
@@ -116,3 +135,16 @@ class postView(Gtk.Box):
 	def load(self, post):
 		url=post['file_url']
 		self.content.load_uri(url)
+		
+		tags={}
+		for key in tagkeys:
+			if key in post:
+				tags[key]=post[key]
+		self.tags.setContent(tags)
+		
+		sources=[post['source']]
+		if 'sources' in post:
+			sources+=post['sources']
+		self.sources.setContent(sources)
+		
+		#self.comments.setContent()
