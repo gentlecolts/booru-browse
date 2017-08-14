efb115d4771aa17f1307b3be9467ec2a77e7ac15
Jack Phoebus <jmp1996.jp@gmail.com>
2017-08-14 16:12:43 -0400

    tags box works now
    it's pretty hacky though...
    cleaning is TODO

--- postView.py
+++ postView.py
@@ -3,43 +3,85 @@ gi.require_version("Gtk", "3.0")
 gi.require_version('WebKit', '3.0')
 from gi.repository import Gtk, WebKit
 
-#tagkeys=['artist', 'tags']
-tagkeys=['tags']
+from tagContainer import tagBox
 
+tagkeys=['artist', 'tags']
+#tagkeys=['tags']
+
+#golden ratio
+golden = (1 + 5 ** 0.5) / 2
+panelWid=int(1280*(1-1/golden)/2)
+
+#TODO: clean this up
 class tagDisplay(Gtk.ScrolledWindow):
-	def __init__(self, tags):
+#class tagDisplay(Gtk.Box):
+	def __init__(self, container, tags:tagBox):
 		super(tagDisplay, self).__init__()
+		#super(tagDisplay, self).__init__(Gtk.Orientation.VERTICAL)
 		self.tags=tags
+		self.parent=container
+		
 		self.box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+		self.box.set_homogeneous(True)
+		#self.box=Gtk.Grid()
+		#self.box.set_column_homogeneous(True)
+		#self.box.set_alignment(0, 0)
 		
 		port=Gtk.Viewport()
 		port.add(self.box)
 		self.add(port)
+		#self.add(self.box)
+		#self.show_all()
+		self.set_size_request(100, 720)
 	
-	def setContent(self, content):
+	def setContent(self, content:dict):
 		"""add stuff to the widget"""
 		#clean anything currently here
 		children=self.box.get_children()
 		for child in children:
 			self.box.remove(child)
 		
+		y=0
+		def addgrid(widget):
+			nonlocal y
+			self.box.attach(widget, 0, y, 1, 1)
+			y+=1
+		
 		from pprint import pprint
 		pprint(content)
-		for category, tags in content.items():
-			self.box.add(Gtk.Label(category))
+		width=0
+		for category, tags in sorted(content.items()):
+			catlab=Gtk.Label(category)
+			catlab.set_alignment(0, 0)
+			self.box.add(catlab)
+			#addgrid(Gtk.Label(category))
+			
 			if type(tags) is str:
 				tags=tags.split()
 			for tag in tags:
 				button=Gtk.LinkButton.new_with_label(tag, tag)
 				def click(button):
-					self.tags.addTags([button.uri])
+					#print(dir(button))
+					self.tags.addTags([button.get_uri()])
 					return True#needed to override default click behavior
 				button.connect('activate-link', click)
+				#button.get_children()[0].set_justify(Gtk.Justification.LEFT)
+				button.set_alignment(0, 0)
+				#button=Gtk.Label(tag)
+				self.box.pack_start(button, expand=True, fill=True, padding=0)
 				
-				self.box.pack_start(button, expand=False, fill=True, padding=0)
+				#self.box.add(button)
+				#addgrid(button)
 				
 				#print("added {}:{}".format(category, tag))
-		self.show_all()
+				width=max(width, button.get_allocation().width)
+				
+				#req=button.get_size_request()
+				#print(req.width, req.height)
+				#print(button.size_request())
+		
+		self.set_size_request(panelWid, self.parent.get_size_request().height)
+		#self.show_all()
 
 class sourceDisplay(Gtk.Box):
 	def __init__(self):
@@ -71,9 +113,11 @@ class postView(Gtk.Box):
 		super(postView, self).__init__(orientation=Gtk.Orientation.VERTICAL)
 		
 		#comments, tags, and sources
+		contentBox=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+		
 		self.sources=sourceDisplay()
 		self.comments=commentDisplay()
-		self.tags=tagDisplay(booruWidget.search.tags)
+		self.tags=tagDisplay(contentBox, booruWidget.search.tags)
 		
 		#web view for displaying media
 		self.content=WebKit.WebView()
@@ -88,7 +132,6 @@ class postView(Gtk.Box):
 		midbox.pack_start(self.sources, expand=False, fill=True, padding=0)
 		midbox.show()
 		
-		contentBox=Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 		contentBox.pack_start(self.tags, expand=False, fill=True, padding=0)
 		contentBox.pack_start(midbox, expand=True, fill=True, padding=0)
 		contentBox.pack_start(self.comments, expand=False, fill=True, padding=0)
@@ -102,7 +145,6 @@ class postView(Gtk.Box):
 		backbtn.set_image(Gtk.Image(stock="gtk-go-back"))
 		backbtn.connect("clicked", lambda btn:booruWidget.closeImage())
 		
-		#TODO: make this accept a "target", switch should hide/show the target
 		def makeToggle(name, target):
 			frame=Gtk.Frame(label=name)
 			button=Gtk.Switch()
@@ -111,6 +153,20 @@ class postView(Gtk.Box):
 			def onswitch(switch, param):
 				if switch.get_active():
 					target.show_all()
+					
+					"""
+					def listchildren(widget, space=''):
+						print(name, space, type(widget))
+						try:
+							children=widget.get_children()
+						except:
+							children=[]
+						space=space+' '
+						for child in children:
+							listchildren(child, space)
+					
+					listchildren(target)
+					"""
 				else:
 					target.hide()
 			
